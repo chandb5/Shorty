dd36513c46493132016cc2658f9f7cf166bc56e8
Chand <chandbud5@gmail.com>
2025-03-14 23:47:09 -0300

    frontend: integrate posthog

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { PostHogProvider } from "posthog-js/react";
 
-createRoot(document.getElementById('root')!).render(
+const POST_HOG_OPTIONS = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={POST_HOG_OPTIONS}
+    >
+      <App />
+    </PostHogProvider>
+  </StrictMode>
+);
