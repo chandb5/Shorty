0231cdf9ab36d490a576de0a4291e47422b5385d
Chand <chandbud5@gmail.com>
2025-04-16 17:44:48 -0300

    clean: remove all build files

--- backend/src/lambda/auth/build/index.py
+++
@@ -1,7 +0,0 @@
-def handler(event, context):
-    """
-    AWS Lambda function to handle authentication events.
-    """
-    print(f"Received event: {event}")
-    print("Hello from Lambda!")
-    return "Hello from Lambda!"
--- backend/src/lambda/shortener/build/index.py
+++
@@ -1,7 +0,0 @@
-def handler(event, context):
-    """
-    AWS Lambda function to handle shortener events.
-    """
-    print(f"Received event: {event}")
-    print("Hello from Shortener!")
-    return "Hello from Shortener!"
