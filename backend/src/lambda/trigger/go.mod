350cfee4d9260b69606fa625259a42484a8014f7
Chand <chandbud5@gmail.com>
2025-04-15 21:00:20 -0300

    add trigger lambda function

---
+++ backend/src/lambda/trigger/go.mod
@@ -0,0 +1,5 @@
+module trigger
+
+go 1.23.4
+
+require github.com/aws/aws-lambda-go v1.48.0 // indirect
---
+++ backend/src/lambda/trigger/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func HandleRequest(ctx context.Context) (Response, error) {
+	return Response{
+		Message: "Hello from trigger lambda!",
+	}, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
