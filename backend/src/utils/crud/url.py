50f2a20195f37d5673604e0c944c3fa7f2a1bcf4
Chand <chandbud5@gmail.com>
2025-04-18 12:28:59 -0300

    feat: add crud operations on URLs

--- backend/src/utils/crud/url.py
+++ backend/src/utils/crud/url.py
@@ -1,13 +1,14 @@
 import uuid
-from utils.db import run_query_fetch, run_query_execute
+from utils.db import run_query_fetch, run_query_fetchrow
 
 
 async def create_short_url_record(slug, url, user_id):
     short_id = str(uuid.uuid4())
-    return await run_query_execute(
+    return await run_query_fetchrow(
         """
         INSERT INTO shortened_urls (id, slug, url, user_id)
         VALUES ($1, $2, $3, $4)
+        RETURNING id, slug, url, user_id
         """,
         short_id, slug, url, user_id
     )
@@ -21,4 +22,57 @@ async def list_short_urls(user_id):
         WHERE user_id = $1
         """,
         user_id
+    )
+
+async def get_short_url_record(long_url=None, slug=None):
+    if slug:
+        return await run_query_fetchrow(
+            """
+            SELECT id, slug, url, user_id
+            FROM shortened_urls
+            WHERE slug = $1
+            """,
+            slug
+        )
+    
+    if long_url:
+        return await run_query_fetchrow(
+            """
+            SELECT id, slug, url, user_id
+            FROM shortened_urls
+            WHERE url = $1
+            """,
+            long_url
+        )
+
+async def update_long_url(slug, new_url, new_slug=None):
+    if new_slug:
+        return await run_query_fetchrow(
+            """
+            UPDATE shortened_urls
+            SET url = $1, slug = $2
+            WHERE slug = $3
+            RETURNING id, slug, url, user_id
+            """,
+            new_url, new_slug, slug
+        )
+    else:
+        return await run_query_fetchrow(
+            """
+            UPDATE shortened_urls
+            SET url = $1
+            WHERE slug = $2
+            RETURNING id, slug, url, user_id
+            """,
+            new_url, slug
+        )
+
+async def delete_short_url(slug):
+    return await run_query_fetchrow(
+        """
+        DELETE FROM shortened_urls
+        WHERE slug = $1
+        RETURNING id, slug, url, user_id
+        """,
+        slug
     )
