92910955488c572bc151c37ba393e771f2d23b3a
Chand <chandbud5@gmail.com>
2025-04-19 19:59:42 -0300

    allow bucket deletion

--- infra/storage.tf
+++ infra/storage.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "shortener-analytics" {
   bucket = "shortener-analytics"
 
-  lifecycle {
-    prevent_destroy = true
-  }
-
   tags = {
     Name        = "shortener-analytics"
   }
